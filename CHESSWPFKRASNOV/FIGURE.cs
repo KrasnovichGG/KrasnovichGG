425be2f8be31e93944e555e24182b6446ff16136
Alexsandr Krasnov <Sasha-KR90@bk.ru>
2021-05-31 23:52:39 +0300

    Поставил на фиксированные координаты Создал класс фигуры(из мудла взял) Сделал ф-ию определения позиции кнопки нажатия и Проверка как ходят фигуры Нужно нажать на фигуру и сделать ход в правильные направления,если сходить неверно все равно нужно верно

---
+++ CHESSWPFKRASNOV/FIGURE.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CHESSWPFKRASNOV
+{
+    class Figure
+    {
+        public int X;
+        public int Y;
+
+        public Figure(int newX, int newY)
+        {
+            X = newX;
+            Y = newY;
+            Console.WriteLine("Figure Constructor");
+        }
+
+        public virtual bool Move(int newX, int newY)
+        {
+            return false;
+        }
+    }
+
+    class King : Figure
+    {
+        public King(int X, int Y) : base(X, Y)
+        {
+            Console.WriteLine("King Constructor");
+        }
+
+        public override bool Move(int newX, int newY)
+        {
+            if (Math.Abs(X - newX) <= 1 && Math.Abs(Y - newY) <= 1)
+            {
+                X = newX;
+                Y = newY;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+
+    class Queen : Figure
+    {
+        public Queen(int X, int Y) : base(X, Y)
+        {
+            Console.WriteLine("Queen Constructor");
+        }
+        public override bool Move(int newX, int newY)
+        {
+            if ((X == newX || Y == newY ||
+            Math.Abs(X - newX) == Math.Abs(Y - newY)))
+            {
+                X = newX;
+                Y = newY;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+
+    class Rook : Figure
+    {
+        public Rook(int X, int Y) : base(X, Y)
+        {
+            Console.WriteLine("Rook Constructor");
+        }
+        public override bool Move(int newX, int newY)
+        {
+            if (X == newX || Y == newY)
+            {
+                X = newX;
+                Y = newY;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    class Bishop : Figure
+    {
+        public Bishop(int X, int Y) : base(X, Y)
+        {
+            Console.WriteLine("Bishop Constructor");
+        }
+        public override bool Move(int newX, int newY)
+        {
+            if (Math.Abs(X - newX) == Math.Abs(Y - newY))
+            {
+                X = newX;
+                Y = newY;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    class Knight : Figure
+    {
+        public Knight(int X, int Y) : base(X, Y)
+        {
+            Console.WriteLine("Knight Constructor");
+        }
+        public override bool Move(int newX, int newY)
+        {
+            if ((Math.Abs(X - newX) == 2 && Math.Abs(Y - newY) == 1) ||
+            (Math.Abs(X - newX) == 1 && Math.Abs(Y - newY) == 2))
+            {
+                X = newX;
+                Y = newY;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+}
--- CHESSWPFKRASNOV/MainWindow.xaml.cs
+++ CHESSWPFKRASNOV/MainWindow.xaml.cs
@@ -20,14 +20,99 @@ namespace CHESSWPFKRASNOV
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Figure> figures = new List<Figure>();
+        bool state = false;
+        Button old;
+        Figure F;
         public MainWindow()
         {
             InitializeComponent();
+            figures.Add(new Queen(4, 3));
+            figures.Add(new Rook(1, 1));
+            figures.Add(new Knight(3, 5));
+            figures.Add(new Bishop(7, 6));
+        }
+        static T FindVisualParent<T>(UIElement element) where T : UIElement
+        {
+            UIElement parent = element;
+            while (parent != null)
+            {
+                T correctlyTyped = parent as T;
+                if (correctlyTyped != null)
+                {
+                    return correctlyTyped;
+                }
+                parent = VisualTreeHelper.GetParent(parent) as UIElement;
+            }
+            return null;
+        }
+        private void Move_Click(object sender, RoutedEventArgs e)
+        {
+            var el = FindVisualParent<Button>(sender as Button);
+            int X = (int)el.GetValue(Grid.RowProperty);
+            int Y = (int)el.GetValue(Grid.ColumnProperty);
+
+            if (F == null)
+            {
+                F = figures.FirstOrDefault(f => f.X == X && f.Y == Y);
+                old = el;
+            }
+            else
+            {
+                int colorInd = F.X + F.Y;
+                bool state = false;
+                switch (F.GetType().Name)
+                {
+                    case "Queen":
+                        state = (F as Queen).Move(X, Y);
+                        if (state)
+                        {
+                            el.Background = old.Background;
+                            F = null;
+                        }
+                        break;
+                    case "Knight":
+                        state = (F as Knight).Move(X, Y);
+                        if (state)
+                        {
+                            el.Background = old.Background;
+                            F = null;
+                        }
+                        break;
+                    case "Bishop":
+                        state = (F as Bishop).Move(X, Y);
+                        if (state)
+                        {
+                            el.Background = old.Background;
+                            F = null;
+                        }
+                        break;
+                    case "Rook":
+                        state = (F as Rook).Move(X, Y);
+                        if (state)
+                        {
+                            el.Background = old.Background;
+                            F = null;
+                        }
+                        break;
+
+                }
+                if (state)
+                {
+                    var bc = new BrushConverter();
+                    old.Background = colorInd % 2 == 0 ? (Brush)bc.ConvertFrom("#E8EBEF") : (Brush)bc.ConvertFrom("#7d8796");
+
+                }
+
+
+            }
         }
 
-        private void x4y4_Click(object sender, RoutedEventArgs e)
+        private void Move_MouseDown(object sender, MouseButtonEventArgs e)
         {
+
 
         }
     }
 }
+
